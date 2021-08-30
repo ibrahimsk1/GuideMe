df4710dca6848f33b2fa976a14cf06c3eabb4222
ibrahimsk1 <ibrahim.sk01@gmail.com>
2021-08-30 23:29:13 +0300

    prod

---
+++ src/constans/config.js
@@ -0,0 +1,11 @@
+export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL
+export const IBB_BASE_URL = process.env.VUE_APP_IBB_BASE_URL
+export const MAP_KEY = process.env.VUE_APP_MAP_KEY
+export const ISOLINE_KEY = process.env.VUE_APP_ISOLINE_KEY
+export const ISOLINE_URL = process.env.VUE_APP_ISOLINE_URL
+
+export const defaultLocale = 'tr'
+export const localeOptions = [
+  { id: 'en', name: 'EN' },
+  { id: 'tr', name: 'TR' },
+]
